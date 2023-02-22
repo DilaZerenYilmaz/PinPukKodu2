ec97dc82cf456a36a6cc769cb7fab97c9ee014d7
dila.yilmaz <d.z.y@hotmail.com>
2023-02-22 23:01:39 +0300

    Pin kodu 3 kez kullanıcıdan alındı

--- PinPukKodu2/Program.cs
+++ PinPukKodu2/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             int pinKodu = 1453, pukKodu = 7894, girilenPuk = 0, girilenPin = 0;
+
+            for (int i = 3; i >= 1; i--)
+            {
+                Console.Write("Pin kodunu giriniz: ");
+                girilenPin = int.Parse(Console.ReadLine());
+
+            }
         }
     }
 }
